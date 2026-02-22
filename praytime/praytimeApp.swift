07d471596a3347aa1ed8212d1e36550376127c2e
Muhammed Ibrahim <stringsaeed@gmail.com>
2026-02-22 19:28:34 +0400

    Initial Commit

---
+++ praytime/praytimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  praytimeApp.swift
+//  praytime
+//
+//  Created by Muhammed Saeed on 22/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct praytimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
